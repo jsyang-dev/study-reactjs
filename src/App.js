7d1f04a69fa4b2257cefab832b8f9ea64c9d94c2
Jeongsu Yang <mycat83@gmail.com>
2023-01-10 23:00:14 +0900

    feat: #7.1 To Do List part one

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 function App() {
-    const [counter, setCounter] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setCounter(prev => prev + 1);
-    const onChange = (event) => setKeyword(event.target.value);
-    useEffect(() => {
-        console.log("I run only once.");
-    }, []);
-    useEffect(() => {
-        if (keyword !== "" && keyword.length > 5) {
-            console.log("I run when 'keyword' changes.");
+    const [toDo, setTodo] = useState("");
+    const [toDos, setTodos] = useState([]);
+    const onChange = (event) => setTodo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
         }
-    }, [keyword]);
-    useEffect(() => {
-        console.log("I run when 'counter' changes.");
-    }, [counter]);
+        setTodos(currentArray => [toDo, ...currentArray]);
+        setTodo("");
+    }
+    console.log(toDos);
     return (
         <div>
-            <input value={keyword}
-                   onChange={onChange}
-                   type="text"
-                   placeholder="Search here..."/>
-            <h1>{counter}</h1>
-            <button onClick={onClick}>Click me</button>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange}
+                       value={toDo}
+                       type="text"
+                       placeholder="Write your to do..."/>
+                <button>Add To Do</button>
+            </form>
         </div>
     );
 }
