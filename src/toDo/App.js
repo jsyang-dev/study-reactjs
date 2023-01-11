15fb5a08dac085860439172cce4aa5c1b7199057
Jeongsu Yang <mycat83@gmail.com>
2023-01-11 22:46:05 +0900

    feat: #7.2 Coin Tracker

---
+++ src/toDo/App.js
@@ -0,0 +1,35 @@
+import {useState} from "react";
+
+function App() {
+    const [toDo, setTodo] = useState("");
+    const [toDos, setTodos] = useState([]);
+    const onChange = (event) => setTodo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setTodos(currentArray => [toDo, ...currentArray]);
+        setTodo("");
+    }
+    return (
+        <div>
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange}
+                       value={toDo}
+                       type="text"
+                       placeholder="Write your to do..."/>
+                <button>Add To Do</button>
+            </form>
+            <hr/>
+            <ul>
+                {toDos.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
